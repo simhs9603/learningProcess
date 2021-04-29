f93df3d83a2926ebbdb23e796b205aa3c77a9e05
simhs9603 <78000215+simhs9603@users.noreply.github.com>
2021-04-29 18:59:43 +0900

    Update C#_MultiTable.cs

--- C#_MultiTable.cs
+++ C#_MultiTable.cs
@@ -1,3 +1,31 @@
+using System;
+
+
+namespace testConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please Enter Number :");
+            string a = Console.ReadLine();///입력은 무조건 string
+            int a1 = Convert.to; ///int 값을 원하면 입력받은 string 은 항상 변환 
+
+
+            if (a1 < 10)
+            { ///입력한 a1의 값이 10 이하 일 때, 
+                for (int b = 1; b < 10; b++) /// b의 값은 10 미만에서 1씩 ++ 
+                {
+                    Console.WriteLine($"{a1}단"); ///$를 사용해 절대 참조
+                    Console.WriteLine($"{a1}*{b}={a1 * b}");
+
+                }
+            }
+        }
+    }
+}
+
+
 
 {
     class Program
